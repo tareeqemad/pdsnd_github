7cd5ddef379b2c5146a274591456b1cd2c2ba109
Tareq Elbawab <tareqelbawab94@gmail.com>
2025-12-24 16:31:23 +0200

    Refactor: clean imports and format constants

--- bikeshare.py
+++ bikeshare.py
@@ -2,20 +2,20 @@ import time
 import pandas as pd
 import numpy as np
 
-CITY_DATA = { 'chicago': 'chicago.csv',
-              'new york city': 'new_york_city.csv',
+CITY_DATA = { 'new york city': 'new_york_city.csv',
+              'chicago': 'chicago.csv',
               'washington': 'washington.csv' }
 
 def get_filters():
     """
-    Asks user to specify a city, month, and day to analyze.
+    Asks user to specify a city, month, and day to analyze  ** refactor.
 
     Returns:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Salam ! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 
 
